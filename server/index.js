0df3de28e8686b142fb942ed5fe0ab24c322f525
daniel-idowu-01 <danielidowu414@gmail.com>
2023-11-13 00:40:22 +0100

    Creation of folders and installations

---
+++ server/index.js
@@ -0,0 +1,46 @@
+const cors = require('cors')
+const express = require('express')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+// database connection
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://danielidowu:<password>@cluster0.lxulz2t.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`)
+})
