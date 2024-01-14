1476a78f3c364a272b87fc5b1566eedc7dec998b
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-14 23:34:28 +0100

    changed profile images of review customers

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -88,7 +88,7 @@ const Review = () => {
                   As a voracious reader, I was thrilled to discover iREAD. The platform's sleek design and easy navigation make it a joy to explore. I appreciate the curated collections and the ability to find both popular titles and hidden gems. It has become my go-to destination for discovering new literary wonders!
                 </p>
                 <Avatar
-                  img={ProfileImage}
+                  img='https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bWVufGVufDB8fDB8fHww'
                   alt="avatar of Jese"
                   rounded
                   className='w-10 mb-4'
@@ -114,7 +114,7 @@ const Review = () => {
                   iREAD is a reader's dream come true! The diverse range of genres and the user-friendly interface make it easy to find exactly what I'm looking for. The reviews and recommendations from fellow readers help me discover books I might have overlooked. This site has reignited my passion for reading.
                 </p>
                 <Avatar
-                  img={ProfileImage}
+                  img='https://images.unsplash.com/photo-1664575602554-2087b04935a5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d29tYW58ZW58MHx8MHx8fDA%3D'
                   alt="avatar of Jese"
                   rounded
                   className='w-10 mb-4'
@@ -140,7 +140,7 @@ const Review = () => {
                   I love the convenience of having an entire library at my fingertips. iREAD has transformed my reading experience. The ability to access my favorite books anytime, anywhere is a game-changer. The site's responsiveness and vast catalog make it an indispensable companion for any book lover.
                 </p>
                 <Avatar
-                  img={ProfileImage}
+                  img='https://t4.ftcdn.net/jpg/02/62/76/57/360_F_262765707_7ipekmhWAQbIy61VGRdpWo4eHeuN6Ub3.jpg'
                   alt="avatar of Jese"
                   rounded
                   className='w-10 mb-4'
@@ -166,9 +166,9 @@ const Review = () => {
                   iREAD has elevated my reading habits. The platform not only houses an extensive collection but also fosters a sense of community among readers. I've found hidden gems and engaged in thoughtful discussions with fellow book enthusiasts. It's more than just a library – it's a vibrant literary community!
                 </p>
                 <Avatar
-                  img={ProfileImage}
+                  img='https://t4.ftcdn.net/jpg/02/62/76/57/360_F_262765707_7ipekmhWAQbIy61VGRdpWo4eHeuN6Ub3.jpg'
                   alt="avatar of Jese"
-                  rounded
+                  rounded 
                   className='w-10 mb-4'
                 />
                 <h5 className='text-lg font-medium'>Lila B.</h5>
