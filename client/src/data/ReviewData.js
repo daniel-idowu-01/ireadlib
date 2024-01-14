c8e60b0b156785ac883cad42d81b59dd894d84cb
daniel-idowu-01 <danielidowu414@gmail.com>
2024-01-14 23:46:04 +0100

    completed the required data

--- client/src/data/ReviewData.js
+++ client/src/data/ReviewData.js
@@ -14,16 +14,22 @@ const data = [
     job: 'Marketer'
   },
   {
-    comment: "iREAD is a literary paradise! The vast selection and intuitive search options make it a breeze to find my favorite reads. The community aspect adds a personal touch – sharing recommendations with fellow book lovers is a delightful bonus. My bookshelf has never been more diverse, thanks to this fantastic e-library!",
-    image: ProfileImage,
-    name: 'Samantha R.',
-    job: 'Accountant'
+    comment: "iREAD is a reader's dream come true! The diverse range of genres and the user-friendly interface make it easy to find exactly what I'm looking for. The reviews and recommendations from fellow readers help me discover books I might have overlooked. This site has reignited my passion for reading.",
+    image: 'https://images.unsplash.com/photo-1664575602554-2087b04935a5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8d29tYW58ZW58MHx8MHx8fDA%3D',
+    name: 'Emily M.',
+    job: 'Software Developer'
   },
   {
-    comment: "iREAD is a literary paradise! The vast selection and intuitive search options make it a breeze to find my favorite reads. The community aspect adds a personal touch – sharing recommendations with fellow book lovers is a delightful bonus. My bookshelf has never been more diverse, thanks to this fantastic e-library!",
-    image: ProfileImage,
-    name: 'Samantha R.',
-    job: 'Accountant'
+    comment: "I love the convenience of having an entire library at my fingertips. iREAD has transformed my reading experience. The ability to access my favorite books anytime, anywhere is a game-changer. The site's responsiveness and vast catalog make it an indispensable companion for any book lover.",
+    image: 'https://t4.ftcdn.net/jpg/02/62/76/57/360_F_262765707_7ipekmhWAQbIy61VGRdpWo4eHeuN6Ub3.jpg',
+    name: 'Jake H.',
+    job: 'Student'
+  },
+  {
+    comment: "iREAD has elevated my reading habits. The platform not only houses an extensive collection but also fosters a sense of community among readers. I've found hidden gems and engaged in thoughtful discussions with fellow book enthusiasts. It's more than just a library – it's a vibrant literary community!",
+    image: 'https://t4.ftcdn.net/jpg/02/62/76/57/360_F_262765707_7ipekmhWAQbIy61VGRdpWo4eHeuN6Ub3.jpg',
+    name: 'Lila B.',
+    job: 'CEO, Volab LTD'
   },
 ]
 
