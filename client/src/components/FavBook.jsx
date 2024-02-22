c15fa56acfef08437aa62ceb54368efa088ce011
daniel-idowu-01 <danielidowu414@gmail.com>
2024-02-22 23:44:15 +0100

    reduced header of favbook

--- client/src/components/FavBook.jsx
+++ client/src/components/FavBook.jsx
@@ -4,19 +4,19 @@ import FavBookImage from '../assets/images/favoritebook.jpg'
 
 const FavBook = () => {
   return (
-    <section className='flex flex-col sm:flex-row justify-between items-center gap-12 px-10 sm:px-20 py-10'>
-      <article className='sm:w-1/2'> 
-        <img src={FavBookImage} alt="" className='rounded sm:w-10/12' />
+    <section className='flex flex-col md:flex-row justify-between items-center gap-12 px-10 md:px-20 py-10'>
+      <article className='md:w-1/2'>
+        <img src={FavBookImage} alt="" className='rounded md:w-10/12' />
       </article>
 
-      <article className='sm:w-1/2'>
-        <h2 className='text-5xl font-bold my-3 sm:w-3/4 leading-snug'>
+      <article className='md:w-1/2'>
+        <h2 className='text-3xl md:text-5xl font-bold my-3 w-[95%] md:w-3/4 leading-snug'>
           Find Your Favorite <span className='text-orange'>Book Here!</span>
         </h2>
         <p className='text-lg'>
           Whether you're a seasoned bibliophile or a casual reader, finding your favorite book has never been more exhilarating. Navigate through our user-friendly interface, seamlessly organized genres, and curated collections to uncover hidden gems and revisit beloved classics. Our advanced search features make it easy to locate that elusive title, author, or genre you've been yearning for.
         </p>
-        <div className='flex flex-col sm:flex-row justify-between gap-6 sm:w-3/4 my-5'>
+        <div className='flex flex-col md:flex-row justify-between gap-6 md:w-3/4 my-5'>
           <div>
             <h3 className='text-3xl font-bold'>800+</h3>
             <p className='text-base'>Book Listing</p>
